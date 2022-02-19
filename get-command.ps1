dc85c7b38ad55527dbda198f071c6a66ec4c8b33
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-02-19 04:39:32 +0300

    Write-Host: Writing a message to the screen

--- get-command.ps1
+++ get-command.ps1
@@ -1,9 +1,13 @@
 
-# Get all PS commands
-Get-Command
+# # Get all PS commands
+# Get-Command
 
-# Get all PS 'GET' commands 
-Get-Command -Verb Get
-Get-Command -Verb Write
+# # Get all PS 'GET' commands 
+# Get-Command -Verb Get
+# Get-Command -Verb Write
 
-Get-Command -Noun Host
+# Get-Command -Noun Host
+
+# Get-ChildItem
+
+Write-Host 'Howdy World'
