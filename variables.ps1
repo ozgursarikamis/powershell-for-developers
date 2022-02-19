adbcb45dcc8177d7a576b93be1b6c73d33db1967
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-02-20 01:16:14 +0300

    variables: definition

---
+++ variables.ps1
@@ -0,0 +1,20 @@
+# devlare a variable
+$variableName = "Ozgur"
+
+$variableName
+
+# Variables are objects. Show the type:
+$variableName.GetType()
+
+$variableName.ToLower()
+$variableName.ToUpper()
+
+$variableName.Length
+
+$members_list = $variableName | Get-Member
+
+$members_list
+
+for ($i = 0; $i -lt $members_list.Count; $i++) {
+	$members_list[$i].Name
+}
