44860795dba2bd5eaa7a0b6a9baca107b19fbb5b
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-02-20 01:41:20 +0300

    strings: escaping characters

--- strings.ps1
+++ strings.ps1
@@ -17,3 +17,13 @@ $sql
 
 # String Interpolation:
 "This is a sentence that contains a variable: $some_text "
+
+# it won't work with single-quotes:
+'This is a sentence that contains a variable: $some_text '
+
+"`t `t This is tabbed in"
+
+"Here is some text `r`n`r`nA blank line before this."
+
+# Escaping a variable name
+"The `$items variable os $items"
