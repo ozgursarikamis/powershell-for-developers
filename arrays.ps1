32010906d3277a042a0c1e55e5f93d0bebbd4673
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-02-20 01:47:56 +0300

    arrays: definition

---
+++ arrays.ps1
@@ -0,0 +1,16 @@
+$array = "Ozgur", "Sarikamis"
+$array
+
+$array.GetType() # System.Array
+
+""
+"`tArray Properties"
+$array.Count
+$array.IsFixedSize
+$array.Length
+$array.Rank
+
+# Indexes
+""
+"`tUsing Array Index"
+$array[0]
