6cf27a39ecc5c95dc45bedf5eb1bc81359825cf5
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-02-20 00:11:47 +0300

    Pipelines: Where-Object

--- 01 - Cmdlets.ps1
+++ 01 - Cmdlets.ps1
@@ -1,5 +1,8 @@
-Set-Location "./Demo"
-Set-Location ".."
-clear
+# Set-Location "./Demo"
+# Set-Location "../../"
+# clear
 
-Get-Help Get-ChildItem -Online
+# Get-Help Get-ChildItem -Online
+
+Get-ChildItem | Where-Object { $_.Length -gt 1kb }
+Get-ChildItem | Where-Object -Property Length -gt 1kb
