244dc2b852feec981e28f7bf1b909134dab23d69
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-02-20 03:09:50 +0300

    functions: definition (detailed)

--- functions.ps1
+++ functions.ps1
@@ -13,3 +13,27 @@ function Get-AValue($one, $two) {
 }
 
 Get-AValue('my name is', 'ozgur')
+
+function Get-BValue {
+	param (
+		[Parameter(Mandatory = $true, HelpMessage = "Please enter value one")] [int] $one,
+		# Parameter help description
+		[Parameter(Mandatory = $false, HelpMessage = "Please enter value two")]
+		[int] $two = 65
+	)
+	begin {
+		Clear-Host
+		"BEGIN"
+	}
+		process {
+			"PROCESS"
+			return $one + $two
+		}
+	end {
+		"END"
+	}
+}
+
+# Get-BValue -one 1 -two 99
+
+Get-BValue -two 2
