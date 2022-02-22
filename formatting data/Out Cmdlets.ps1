17a88c69057c7a26e4cb7a8968b24e70aa30b150
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2022-02-23 00:47:39 +0300

    Out cmdlets notes

---
+++ formatting data/Out Cmdlets.ps1
@@ -0,0 +1,6 @@
+# Out cmdlets do not format objects
+# Pipe data to an out cmdlet, or use the InputObject parameter
+# Except for Out-String, out cmdlets don't output objects to the pipeline
+# When using out cmdlets, your declaring that you're done with objects.
+
+## Always put out cmdlets at the end of the pipeline.
