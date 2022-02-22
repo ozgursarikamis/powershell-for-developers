3a478ef3debebfe378faf22affbd521d5532ad26
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-02-22 22:47:54 +0300

    Format-Table

--- formatting data/01-Get-Member.ps1
+++ formatting data/01-Get-Member.ps1
@@ -1,18 +1,26 @@
 $myString = "this is a string of text. nothing too exciting"
 
-# Does nothing much. Since it's just a string
-$myString | Format-Table
-$myString | Format-List
+# # Does nothing much. Since it's just a string
+# $myString | Format-Table
+# $myString | Format-List
 
-Clear-Host
+# Clear-Host
 
-Get-NetAdapter
-""
-Get-NetAdapter | Out-Host
+# Get-NetAdapter
+# ""
+# Get-NetAdapter | Out-Host
 
-Get-NetAdapter | Out-Default | Out-Host
+# Get-NetAdapter | Out-Default | Out-Host
 
 # Almost identical because `Out-Host` was already commanded implicitly
 
 # Clear-Host
+# Get-Process | Get-Member | more
 
+
+Clear-Host
+
+$formatData = Get-FormatData -TypeName System.Diagnostics.Process -PowerShellVersion 5.1
+
+$formatData.FormatViewDefinition[0].Control
+$formatData.FormatViewDefinition[0].Control.Headers | Format-Table
