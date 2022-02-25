99cd2f235508d548b877ec7aa5508c6356b75a9f
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-02-25 00:37:44 +0000

    Get-Command -Verb

--- 02 - extending powershell/03 - Identifying Commands.ps1
+++ 02 - extending powershell/03 - Identifying Commands.ps1
@@ -7,3 +7,14 @@
 Clear-Host
 
 Get-Command -Module BitsTransfer
+
+Clear-Host
+
+Get-Module BitsTransfer
+
+Clear-Host
+
+(Get-Module BitsTransfer).ExportedCommands
+
+Clear-Host
+Get-Command -Module BitsTransfer -Verb GET
