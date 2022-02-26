41b8fdf0434530ebbd1eec74a77397f2f1f818b7
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-02-26 23:44:08 +0000

    Install-Module

--- 02 - extending powershell/04 - Package Management.ps1
+++ 02 - extending powershell/04 - Package Management.ps1
@@ -1,49 +1,51 @@
-# Get-Command -Module PackageManagement
+# # Get-Command -Module PackageManagement
 
+# # Clear-Host
+
+# # Get-PackageProvider | Format-Table
+
+# # Get-Command -Module PowerShellGet
+
+# # Clear-Host
+
+# # Get-PSRepository | Format-Table
+
+# # New-Item -Path C:\LocalPSRepo -ItemType Directory
+# #TODO: check if directory exists
+
+# New-SmbShare -Name LocalPSRepo -Path C:\LocalPSRepo
+
+# Register-PSRepository -Name LocalPSRepo -SourceLocation \\localhost\LocalPSRepo\ -ScriptSourceLocation \\localhost\LocalPSRepo\ -InstallationPolicy Trusted
+
+# Get-PSRepository
+
+# ### Outpur
+
+# # Name                      InstallationPolicy   SourceLocation
+# # ----                      ------------------   --------------
+# # LocalPSRepo               Trusted              \\localhost\LocalPSRepo\
+# # PSGallery                 Untrusted            https://www.powershellgallery.com/api/v2
+
+# Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
+
+# # Name                      InstallationPolicy   SourceLocation
+# # ----                      ------------------   --------------
+# # PSGallery                 Trusted              https://www.powershellgallery.com/api/v2
+# # LocalPSRepo               Trusted              \\localhost\LocalPSRepo\
+
+# Find-Module -Name "*Slack*" # Search by name
 # Clear-Host
 
-# Get-PackageProvider | Format-Table
+# Find-Module -Tag "Slack" # Search by tag
 
-# Get-Command -Module PowerShellGet
+# Find-Module -Name PSSlack -AllVersions
+# Find-Module -Name PSSlack -MaximumVersion 1.0
+# Find-Module -Name PSSlack -RequiredVersion 1.0.2
 
-# Clear-Host
+#  Clear-Host
 
-# Get-PSRepository | Format-Table
+#  Find-Command -ModuleName PSSlack
 
-# New-Item -Path C:\LocalPSRepo -ItemType Directory
-#TODO: check if directory exists
+#  Find-Command -Name Get-VM
 
-New-SmbShare -Name LocalPSRepo -Path C:\LocalPSRepo
-
-Register-PSRepository -Name LocalPSRepo -SourceLocation \\localhost\LocalPSRepo\ -ScriptSourceLocation \\localhost\LocalPSRepo\ -InstallationPolicy Trusted
-
-Get-PSRepository
-
-### Outpur
-
-# Name                      InstallationPolicy   SourceLocation
-# ----                      ------------------   --------------
-# LocalPSRepo               Trusted              \\localhost\LocalPSRepo\
-# PSGallery                 Untrusted            https://www.powershellgallery.com/api/v2
-
-Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-
-# Name                      InstallationPolicy   SourceLocation
-# ----                      ------------------   --------------
-# PSGallery                 Trusted              https://www.powershellgallery.com/api/v2
-# LocalPSRepo               Trusted              \\localhost\LocalPSRepo\
-
-Find-Module -Name "*Slack*" # Search by name
-Clear-Host
-
-Find-Module -Tag "Slack" # Search by tag
-
-Find-Module -Name PSSlack -AllVersions
-Find-Module -Name PSSlack -MaximumVersion 1.0
-Find-Module -Name PSSlack -RequiredVersion 1.0.2
-
- Clear-Host
-
- Find-Command -ModuleName PSSlack
-
- Find-Command -Name Get-VM
+Install-Module -Name PSSlack
