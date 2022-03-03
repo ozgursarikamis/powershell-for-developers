b03fb5391a81e0b78d9848da198d002d86aa1e7b
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 01:35:11 +0300

    Windows Popup operations: Changing FG and BG colors

--- 03 - Automation with Windows Powershell Scripts/Powershell Fun - Other.ps1
+++ 03 - Automation with Windows Powershell Scripts/Powershell Fun - Other.ps1
@@ -6,9 +6,12 @@
 # $wsh.Popup("Isn't this fun?", 10, "Powershell Automation", 0+64)
 # $wsh.Popup("Failed to do something. Do you want to try again?", -1, "Script Error", 4+32)
 
-$Host.UI.RawUI.WindowTitle
+# $Host.UI.RawUI.WindowTitle
 
-foreach ($item in 1..5) {
-	$Host.UI.RawUI.WindowTitle = $item
-	Start-Sleep -Seconds 1
-}
+# foreach ($item in 1..5) {
+# 	$Host.UI.RawUI.WindowTitle = $item
+# 	Start-Sleep -Seconds 1
+# }
+
+$host.UI.RawUI.BackgroundColor = "White"
+$host.UI.RawUI.ForegroundColor = "Magenta"
