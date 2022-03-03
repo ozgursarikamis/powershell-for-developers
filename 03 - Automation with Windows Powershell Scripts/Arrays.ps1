5ea0e145d0b3c8d8c9c34fe9243bf6126be448cb
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 00:27:03 +0300

    Arrays

---
+++ 03 - Automation with Windows Powershell Scripts/Arrays.ps1
@@ -0,0 +1,16 @@
+$n = 1..10
+$n
+"=================="
+$n.Count
+
+$arr = @() # empty array
+
+$arr += 1..5
+
+$arr
+
+$comma_separated_array = "jeff", "jason", "don", "adam", "tim"
+
+$comma_separated_array
+
+$comma_separated_array -is [array]
