5378b16c64e8771d10518793536df7196b997717
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 01:33:07 +0300

    Windows Popup operations: Terminal / Window Title

--- 03 - Automation with Windows Powershell Scripts/Powershell Fun - Other.ps1
+++ 03 - Automation with Windows Powershell Scripts/Powershell Fun - Other.ps1
@@ -1,7 +1,14 @@
-"Accessing .NET COM Objects"
+# "Accessing .NET COM Objects"
 
-$wsh = New-Object -ComObject wscript.shell
-$wsh.Popup.OverloadDefinitions
+# $wsh = New-Object -ComObject wscript.shell
+# $wsh.Popup.OverloadDefinitions
 
-$wsh.Popup("Isn't this fun?", 10, "Powershell Automation", 0+64)
-$wsh.Popup("Failed to do something. Do you want to try again?", -1, "Script Error", 4+32)
+# $wsh.Popup("Isn't this fun?", 10, "Powershell Automation", 0+64)
+# $wsh.Popup("Failed to do something. Do you want to try again?", -1, "Script Error", 4+32)
+
+$Host.UI.RawUI.WindowTitle
+
+foreach ($item in 1..5) {
+	$Host.UI.RawUI.WindowTitle = $item
+	Start-Sleep -Seconds 1
+}
