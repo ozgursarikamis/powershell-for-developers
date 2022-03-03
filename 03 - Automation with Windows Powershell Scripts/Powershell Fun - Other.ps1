f9b0fb66a59b183142c089cce99552e08e5c0532
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 01:42:19 +0300

    Windows Popup operations: Coloring Table Data

--- 03 - Automation with Windows Powershell Scripts/Powershell Fun - Other.ps1
+++ 03 - Automation with Windows Powershell Scripts/Powershell Fun - Other.ps1
@@ -13,5 +13,15 @@
 # 	Start-Sleep -Seconds 1
 # }
 
-$host.UI.RawUI.BackgroundColor = "White"
-$host.UI.RawUI.ForegroundColor = "Magenta"
+# $host.UI.RawUI.BackgroundColor = "White"
+# $host.UI.RawUI.ForegroundColor = "Magenta"
+
+Get-Service | ForEach-Object {
+	if ($_.Status -eq 'Stopped') {
+		$host.UI.RawUI.ForegroundColor = "Red"
+	} else {
+		$host.UI.RawUI.ForegroundColor = "White"
+	}
+
+	$_
+} | Format-Table
