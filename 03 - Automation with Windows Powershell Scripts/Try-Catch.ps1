d2204608bc1fd6367cd3cc2bd8fbad92c91fc17f
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 00:50:14 +0300

    Try-Catch with multiple catches

--- 03 - Automation with Windows Powershell Scripts/Try-Catch.ps1
+++ 03 - Automation with Windows Powershell Scripts/Try-Catch.ps1
@@ -16,3 +16,16 @@ catch {
 finally {
 	"THIS IS FINALLY BLOCK"
 }
+Clear-Host
+
+try {
+	$wc = New-Object System.Net.WebClient
+	$wc.DownloadFile("http://www.contoso.com/MyDoc.doc", 'C:\Scripts\Folder 1')
+	
+}
+catch [System.Net.WebException], [System.IO.IOException] {
+	Write-Error "One of these exceptions happened [System.Net.WebException], [System.IO.IOException]"
+}
+catch {
+	"An error occurred that could not be resolved."
+}
