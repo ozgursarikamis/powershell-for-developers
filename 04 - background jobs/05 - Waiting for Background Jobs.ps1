659ef8292eddf4904a2d769bad8a4d08f9ce5355
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 19:42:08 +0300

    get job details and results

--- 04 - background jobs/05 - Waiting for Background Jobs.ps1
+++ 04 - background jobs/05 - Waiting for Background Jobs.ps1
@@ -14,3 +14,15 @@ $job | Wait-Job
 
 # Get-Job		 : Gets PS BACKGROUND JOBS that are running in the current session
 # Receive-Job	 : Gets the RESULTS of the PS background jobs in the current session
+
+# Retrieve job details:
+$job = Start-Job -ScriptBlock { Get-Process } -Name "Job1"
+
+Get-Job -IncludeChildJob
+Get-Job -IncludeChildJob -Name "Job2"
+
+Receive-Job -Id 5
+
+# The Receive-Job cmdlet gets the results of PowerShell background jobs
+$job = Start-Job -ScriptBlock { $env:COMPUTERNAME }
+Receive-Job -Job $job
