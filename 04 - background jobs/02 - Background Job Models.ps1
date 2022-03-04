48412705978907d379f08e671c607b1ceee37dc3
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 03:19:12 +0300

    Get-Job,  Remove-Job, Receive-Job

--- 04 - background jobs/02 - Background Job Models.ps1
+++ 04 - background jobs/02 - Background Job Models.ps1
@@ -4,3 +4,15 @@ Start-Job -ScriptBlock {
 	# Do something.
 	Get-Process
  }
+
+ Start-Job -ScriptBlock {
+	 Get-Process
+ } -Name "TestJob" -Verbose
+
+ Get-Job -Id 11
+ Remove-Job -Id 11
+
+
+ Receive-Job -Id 19
+# Gets the results of the PowerShell background jobs in the current session.
+
