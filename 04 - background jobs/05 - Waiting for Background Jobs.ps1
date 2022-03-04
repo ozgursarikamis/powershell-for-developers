1ce70ea744c52278e1de7d98a4831193cc818a4c
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 14:57:26 +0300

    Receive-Jobs

--- 04 - background jobs/05 - Waiting for Background Jobs.ps1
+++ 04 - background jobs/05 - Waiting for Background Jobs.ps1
@@ -10,3 +10,7 @@ Wait-Job -Id 1,2,5
 $session = New-PSSession -ComputerName localhost
 $job = Invoke-Command -Session $session -ScriptBlock { Get-Process } -AsJob
 $job | Wait-Job
+
+
+# Get-Job		 : Gets PS BACKGROUND JOBS that are running in the current session
+# Receive-Job	 : Gets the RESULTS of the PS background jobs in the current session
