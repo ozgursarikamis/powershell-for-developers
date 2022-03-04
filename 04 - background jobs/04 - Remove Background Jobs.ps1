10f10d323bbfcf972c6b01e749e8496ae343e45b
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 13:16:10 +0300

    creating a session, creating a job for another session

--- 04 - background jobs/04 - Remove Background Jobs.ps1
+++ 04 - background jobs/04 - Remove Background Jobs.ps1
@@ -11,3 +11,10 @@ Get-Job -Id 1
 Remove-Job -Id 1 # Remove by id
 
 Remove-Job -InstanceId 64e3bf65-9ba3-11ec-a5ff-c4a77d045798 # Remove by instance id
+
+
+# CREATE A NEW SESSION
+$session = New-PSSession -ComputerName localhost
+$session
+
+Invoke-Command -Session $session -ScriptBlock { Start-Job -ScriptBlock { Get-Process } -Name "LocalhostJob" }
