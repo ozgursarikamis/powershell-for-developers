46841031e55245d08a80a96ffd77bd0c40093bee
Ozgur Sarikamis <ozgur.sarikamis@smoothwall.com>
2022-03-04 12:54:21 +0300

    3 ways to create jobs

---
+++ 04 - background jobs/03 - Creating Background Jobs.ps1
@@ -0,0 +1,12 @@
+# Creating Background Job using "Start-Job"
+
+Start-Job -ScriptBlock { Get-Process -Name notepad }
+
+# Create Background Job using 'Ampersand' (&) operator
+Get-Process -Name notepad &
+
+# Create Background Job using "Invoke-Command" and "-AsJob" Parameter
+
+Invoke-Command -ComputerName OFFICE -ScriptBlock { Get-Process -Name notepad } -JobName "Notepad" -AsJob # RemoteJob
+
+Get-Process -Name notepad & # BackgroundJob
